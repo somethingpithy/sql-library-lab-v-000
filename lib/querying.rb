948c8d9781a80516a964165a95a810db42aff76e
Carlota Bindner <cev.bindner@gmail.com>
2018-03-15 18:34:24 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title,
+  "SELECT series.title
   FROM series
   JOIN characters
   ON character.series_id = series.id
