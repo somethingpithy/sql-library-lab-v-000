dc48aa23cd3290558e1084816c8f02cd5eef2f9a
Carlota Bindner <cev.bindner@gmail.com>
2018-03-15 18:39:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -44,5 +44,5 @@ def select_character_names_and_number_of_books_they_are_in
   JOIN characters
   ON character_books.character_id = characters.id
   GROUP BY characters.name
-  ORDER BY book_count ASC"
+  ORDER BY book_count DESC"
 end
