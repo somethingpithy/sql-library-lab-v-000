9743e1423b2cdfedee4443d8f4291e5b16f1317c
Carlota Bindner <cev.bindner@gmail.com>
2018-03-15 17:48:12 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,14 +1,12 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES
-  (1, 'The Magician\'s Trilogy', 1, 1),
-  (2, 'The Hitchhiker\'s Guide to the Galaxy', 2, 2);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, 'The Magician\'s Trilogy', 1, 1);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (2, 'The Hitchhiker\'s Guide to the Galaxy', 2, 2);
 
-INSERT INTO books (id, title, year, series_id) VALUES
-  (1, 'The Magicians', 2009, 1),
-  (2, 'The Magician King', 2011, 1),
-  (3, 'The Magician\'s Land', 2014, 1),
-  (4, 'The Hitchhiker\'s Guide to the Galaxy', 1979, 2),
-  (5, 'The Restaurant at the End of the Universe', 1980, 2),
-  (6, 'Life, the Universe and Everything', 1982, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (1, 'The Magicians', 2009, 1);
+INSERT INTO books (id, title, year, series_id) VALUES (2, 'The Magician King', 2011, 1);
+INSERT INTO books (id, title, year, series_id) VALUES (3, 'The Magician\'s Land', 2014, 1);
+INSERT INTO books (id, title, year, series_id) VALUES (4, 'The Hitchhiker\'s Guide to the Galaxy', 1979, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (5, 'The Restaurant at the End of the Universe', 1980, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (6, 'Life, the Universe and Everything', 1982, 2);
 
 INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES (1, 'Quentin Coldwater', 'human', 'I got my heart\'s desire, and there my troubles began.', 1, 1);
 INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES (2, 'Alice Quinn', 'human', NULL, 1, 1);
