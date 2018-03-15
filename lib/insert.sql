6aee4c613e33054f6966ab6cb35d68040d00102a
Carlota Bindner <cev.bindner@gmail.com>
2018-03-15 17:39:12 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,3 @@
-inserting
-  has 6 books (FAILED - 2)
-  has 8 characters (FAILED - 3)
-  has 2 subgenres (FAILED - 4)
-  has 2 authors (FAILED - 5)
   has 16 joins in character_books (FAILED - 6)
 
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES
@@ -12,7 +7,7 @@ INSERT INTO series (id, title, author_id, subgenre_id) VALUES
 INSERT INTO books (id, title, year, series_id) VALUES
   (1, 'The Magicians', 2009, 1);
   (2, 'The Magician King', 2011, 1);
-  (3, 'Thje Magician\'s Land', 2014, 1);
+  (3, 'The Magician\'s Land', 2014, 1);
   (4, 'The Hitchhiker\'s Guide to the Galaxy', 1979, 2);
   (5, 'The Restaurant at the End of the Universe', 1980, 2);
   (6, 'Life, the Universe and Everything', 1982, 2);
@@ -37,19 +32,19 @@ INSERT INTO authors (id, name) VALUES
 
 
 INSERT INTO character_books (id, book_id, character_id) VALUES
-  (1, );
-  (2, );
-  (3, );
-  (4, );
-  (5, );
-  (6, );
-  (7, );
-  (8, );
-  (9, );
-  (10, );
-  (11, );
-  (12, );
-  (13, );
-  (14, );
-  (15, );
-  (16, ):
+  (1, 1, 1);
+  (2, 1, 2);
+  (3, 1, 4);
+  (4, 2, 1);
+  (5, 3, 1);
+  (6, 3, 2);
+  (7, 4, 5);
+  (8, 4, 6);
+  (9, 4, 7);
+  (10, 4, 8);
+  (11, 5, 5);
+  (12, 5, 6);
+  (13, 6, 5);
+  (14, 6, 6);
+  (15, 7, 5);
+  (16, 7, 6):
