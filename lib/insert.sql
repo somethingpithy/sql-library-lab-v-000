c3aa302681f7f4b63a40b56fdbff19d5bf235d01
Carlota Bindner <cev.bindner@gmail.com>
2018-03-15 14:58:03 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,55 @@
+inserting
+  has 2 series (FAILED - 1)
+  has 6 books (FAILED - 2)
+  has 8 characters (FAILED - 3)
+  has 2 subgenres (FAILED - 4)
+  has 2 authors (FAILED - 5)
+  has 16 joins in character_books (FAILED - 6)
+
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES
+  (1, );
+  (2, );
+
+INSERT INTO books (id, title, year, series_id) VALUES
+  (1, );
+  (2, );
+  (3, );
+  (4, );
+  (5, );
+  (6, );
+
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES
+  (1, );
+  (2, );
+  (3, );
+  (4, );
+  (5, );
+  (6, );
+  (7, );
+  (8, );
+
+INSERT INTO subgenres (id, name) VALUES
+  (1, );
+  (2, );
+
+INSERT INTO authors (id, name) VALUES
+  (1, );
+  (2, );
+
+INSERT INTO character_books (id, book_id, character_id) VALUES
+  (1, );
+  (2, );
+  (3, );
+  (4, );
+  (5, );
+  (6, );
+  (7, );
+  (8, );
+  (9, );
+  (10, );
+  (11, );
+  (12, );
+  (13, );
+  (14, );
+  (15, );
+  (16, ):
